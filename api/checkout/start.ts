7ab2dc95a2acf20ea454863fb7327d8fa0d0edf3
Your Name <pranavgr18@gmail.com>
2025-11-11 15:31:24 +0530

    update

--- api/checkout/start.ts
+++ api/checkout/start.ts
@@ -4,7 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.VITE_SUPABASE_URL!,
-  process.env.VITE_SUPABASE_PUBLISHABLE_KEY!
+  process.env.VITE_SUPABASE_ANON_KEY!
 );
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
